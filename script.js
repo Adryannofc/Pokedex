bfa14643b1f0650de63b8367df011ca39a1a8d2f
Adryann Felix <adryannofc@gmail.com>
2025-09-09 13:59:28 -0300

    feat(pokeApi): adiciona funcao getPokemon para buscar dados

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 import {getPokemon} from './modules/api.js';
 import {createPokemonCard} from './modules/ui.js'
 import {formatName} from './modules/utils.js'
+
+// Modulo para requisicao do Pokemon
+export async function getPokemon(identifier) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`);
+
+        if(!response){
+            throw new Error('Pokemon não encontrado');
+        } 
+
+        const data = await response.json();
+        return data; // devolve o objeto {Pokemon}
+    } catch (error) {
+        console.error(error);
+        return null
+    }
+}
